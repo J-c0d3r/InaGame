cfbea12afe7756dd306a4c8c12dc9726d8a5e048
J-c0d3r <juan.ferrugini@ges.inatel.br>
2023-10-25 16:57:30 -0300

    v0.0.1.4.1 - texts in interface

---
+++ InaGame_Project/Assets/Scripts/MG_Bolsa/SpawnPoints.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+    public List<GameObject> spawnPointList = new List<GameObject>();
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+   
+}
